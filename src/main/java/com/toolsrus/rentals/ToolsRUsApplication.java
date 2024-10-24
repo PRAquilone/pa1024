c465e5892db1ad8f7cc0ccbb2fe8502cc06c8c7c
pat.aquilone <praquilone@gmail.com>
2024-10-24 17:19:03 -0500

    Initial commit with just database stuff

---
+++ src/main/java/com/toolsrus/rentals/ToolsRUsApplication.java
@@ -0,0 +1,13 @@
+package com.toolsrus.rentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToolsRUsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToolsRUsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/toolsrus/rentals/ToolsRUsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toolsrus.rentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToolsRUsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
