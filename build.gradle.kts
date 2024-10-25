dde777164ea8b7c8609719243b752ebed6c2f2ea
pat.aquilone <praquilone@gmail.com>
2024-10-25 00:18:17 -0500

    Fix sql

--- build.gradle.kts
+++ build.gradle.kts
@@ -24,8 +24,9 @@ repositories {
 }
 
 dependencies {
-//	implementation("org.springframework.boot:spring-boot-starter-data-jdbc")
-	implementation("org.springframework.boot:spring-boot-starter-data-jpa")
+	implementation("org.springframework.boot:spring-boot-starter-data-jpa:3.3.4")
+	implementation("org.springframework.boot:spring-boot-starter:3.3.4")
+	implementation("org.springframework.boot:spring-boot-starter-web:3.3.4")
 	compileOnly("org.projectlombok:lombok")
 	runtimeOnly("com.h2database:h2")
 	annotationProcessor("org.projectlombok:lombok")
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -2,109 +2,108 @@
 -- This is expected to be replaced with an actual database
 --
 -- Create Schema for use
-CREATE SCHEMA ToolsSchema
-USE ToolsSchema
+CREATE SCHEMA ToolsSchema;
+USE ToolsSchema;
 
 -- Create the tool type table for listing the tool types
-CREATE TABLE toolType {
+CREATE TABLE toolType (
     type varchar(20) NOT NULL PRIMARY KEY
-}
+);
 
 -- Create the vendor brands table for listing the brands
-CREATE TABLE vendors {
+CREATE TABLE vendors (
     brand varchar(30) NOT NULL PRIMARY KEY
-}
+);
 
 -- Create the tools table for listing the tools
-CREATE TABLE tools {
+CREATE TABLE tools (
     toolCode varchar(10) NOT NULL PRIMARY KEY,
-    toolType varchar(20) NOT NULL FOREIGN KEY REFERENCES toolType(type),
-    toolBrand varchar(30) NOT NULL FOREIGN KEY REFERENCES vendors(brand)
-}
+    toolType varchar(20) NOT NULL REFERENCES toolType(type),
+    toolBrand varchar(30) NOT NULL REFERENCES vendors(brand)
+);
 
 -- Create the charge table based on tool type
-CREATE TABLE toolCharge {
-    toolType varchar(20) NOT NULL PRIMARY KEY FOREIGN KEY REFERENCES toolType(type),
+CREATE TABLE toolCharge (
+    toolType varchar(20) NOT NULL PRIMARY KEY REFERENCES toolType(type),
     dailyCharge decimal(5,2),
     weekDayCharge bit,
     weekEndCharge bit,
     holidayCharge bit
-}
+);
 
 -- Create the holiday day falls on table
-CREATE TABLE days {
+CREATE TABLE days (
     dayOfWeek varchar(15) NOT NULL PRIMARY KEY
-}
+);
 
 -- Create the allowed frequencies of holiday table
-CREATE TABLE frequency {
+CREATE TABLE frequency (
     freq varchar(20) NOT NULL PRIMARY KEY
-}
+);
 
 -- Create the holidays table for checking if it is a holiday
-CREATE TABLE holiday {
+CREATE TABLE holiday (
     name varchar(20) NOT NULL PRIMARY KEY,
     holidayMonth int,
     holidayDay int,
-    dayFallsOn varchar(15) FOREIGN KEY REFERENCES days(dayOfWeek),
-    frequency varchar(20) FOREIGN KEY REFERENCES frequency(freq)
-}
+    dayFallsOn varchar(15) REFERENCES days(dayOfWeek),
+    frequency varchar(20) REFERENCES frequency(freq)
+);
 
 -- Create Rental table for tracking when a tool is rented
-CREATE TABLE rental {
-    toolCode varchar(10) NOT NULL PRIMARY KEY FOREIGN KEY REFERENCES tools(toolCode),
-    toolType varchar(20) NOT NULL PRIMARY KEY FOREIGN KEY REFERENCES toolType(type),
-    toolBrand varchar(30) NOT NULL PRIMARY KEY FOREIGN KEY REFERENCES vendors(brand),
+CREATE TABLE rental (
+    toolCode varchar(10) NOT NULL PRIMARY KEY REFERENCES tools(toolCode),
+    toolType varchar(20) NOT NULL REFERENCES toolType(type),
+    toolBrand varchar(30) NOT NULL REFERENCES vendors(brand),
     rentalDays int NOT NULL,
     checkOutDate date NOT NULL,
     dueDate date,
     chargeDays int NOT NULL,
     due decimal(10,2) NOT NULL,
-    dailyCharge decimal(5,2) NOT NULL FOREIGN KEY REFERENCES toolCharge(dailyCharge),
-    chargeDays int NOT NULL,
+    dailyCharge decimal(5,2) NOT NULL,
     preDiscountCharge decimal(10,2) NOT NULL,
     discountPercent decimal(3,3),
     discountAmount decimal(10,2),
     finalCharge decimal(10,2) NOT NULL
-}
+);
 
 -- Populate the tool types
 INSERT INTO toolType (type)
-    VALUES ("ChainSaw"),
-           ("JackHammer"),
-           ("Ladder");
+    VALUES ('ChainSaw'),
+           ('JackHammer'),
+           ('Ladder');
 
 -- Populate the vendor brands table
-INSERT INTO vendor (brand)
-    VALUES ("Stihl"),
-           ("Werner"),
-           ("DeWalt"),
-           ("Ridgid");
+INSERT INTO vendors (brand)
+    VALUES ('Stihl'),
+           ('Werner'),
+           ('DeWalt'),
+           ('Ridgid');
 
 -- Populate the Tools table
 INSERT INTO tools (toolCode, toolType, toolBrand)
-    VALUES ("CHNS", "ChainSaw", "Stihl"),
-           ("LADW", "Ladder", "Werner"),
-           ("JAKD", "Jackhammer", "DeWalt"),
-           ("JAKR", "Jackhammer", "Ridgid");
+    VALUES ('CHNS', 'ChainSaw', 'Stihl'),
+           ('LADW', 'Ladder', 'Werner'),
+           ('JAKD', 'JackHammer', 'DeWalt'),
+           ('JAKR', 'JackHammer', 'Ridgid');
 
 -- Populate the charge table
-INSERT INTO charge ( toolType, dailyCharge, weekDayCharge, weekEndCharge, holidayCharge)
-    VALUES ("Ladder", 1.99, 1, 1, 0),
-           ("Chainsaw", 1.49, 1, 0, 1),
-           ("Jackhammer", 2.99, 1, 0, 0);
+INSERT INTO toolCharge ( toolType, dailyCharge, weekDayCharge, weekEndCharge, holidayCharge)
+    VALUES ('Ladder', 1.99, 1, 1, 0),
+           ('ChainSaw', 1.49, 1, 0, 1),
+           ('JackHammer', 2.99, 1, 0, 0);
 
 -- Populate the days of the week a holiday can fall on
 INSERT INTO days (dayOfWeek)
-   VALUES ("Monday");
+   VALUES ('Monday');
 
 -- Populate the frequencies for holidays allowed
 INSERT INTO frequency (freq)
-    VALUES ("first");
+    VALUES ('First');
 
 -- Populate the holidays table
 INSERT INTO holiday (name, holidayMonth, holidayDay, dayFallsOn, frequency)
-    VALUES ("Independence Day", 7, 4, null, null),
-           ("Labor Day", null, null, "Monday", "First");
+    VALUES ('Independence Day', 7, 4, null, null),
+           ('Labor Day', null, null, 'Monday', 'First');
 
 
